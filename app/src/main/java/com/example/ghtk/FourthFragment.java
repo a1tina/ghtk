5f1064a652eed6f0a2c49abef04ac1ecf863b535
nenechiii <khoi2809@gmail.com>
2021-06-07 20:49:55 +0700

    1st

---
+++ app/src/main/java/com/example/ghtk/FourthFragment.java
@@ -0,0 +1,116 @@
+package com.example.ghtk;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FourthFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FourthFragment extends Fragment {
+
+    public static TextView profileName;
+    public TextView profileEmail;
+    public ImageView profileImage;
+    private AppCompatButton logoutBtn;
+    private FirebaseAuth firebaseAuth;
+
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FourthFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FourthFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FourthFragment newInstance(String param1, String param2) {
+        FourthFragment fragment = new FourthFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_fourth, container, false);
+        profileName = (TextView) view.findViewById(R.id.tv_profilename);
+        profileEmail = (TextView) view.findViewById((R.id.tv_email));
+        profileImage = (ImageView) view.findViewById(R.id.iv_avatar);
+        logoutBtn = (AppCompatButton) view.findViewById(R.id.acb_logout);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
+        return view;
+    }
+
+
+    private void checkUser() {
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            String name = firebaseUser.getDisplayName();
+            String email = firebaseUser.getEmail();
+            String imgurl = firebaseUser.getPhotoUrl().toString();
+            Glide.with(getActivity()).load(imgurl).into(profileImage);
+            profileName.setText(name);
+            profileEmail.setText(email);
+        }
+        else{
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+        }
+
+    }
+}
